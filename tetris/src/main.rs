74780030a73f5c48a5a13ac30d9d1f7faac3bbf8
J.Z. Zhou <79814775+jzho987@users.noreply.github.com>
2024-06-16 16:43:21 +1200

    update render

--- tetris/src/main.rs
+++ tetris/src/main.rs
@@ -29,13 +29,40 @@ fn main() {
     let duration = Duration::from_millis(frame_time_millis as u64);
     let mut drop_timer = 0;
     let drop_time_millis = 100;
-
     enable_raw_mode().unwrap();
 
+    execute!(stdout, Clear(ClearType::All), cursor::MoveTo(0, 0)).unwrap();
+    // render border
+    {
+        for i in 0..=15 {
+            for j in 0..=21 {
+                let cell = get_cell(&3);
+                let row_pos = (2 * i) as u16;
+                execute!(stdout, cursor::MoveTo(row_pos, j), Print(cell)).unwrap();
+            }
+        }
+    }
     loop {
-        // render current grid state;
+        // render (13, 1) to (15, 2) upcoming and stored.
         {
-            execute!(stdout, Clear(ClearType::All), cursor::MoveTo(0, 0)).unwrap();
+            for i in 12..=14 {
+                for j in 1..=2 {
+                    let cell = get_cell(&0);
+                    let row_pos = (2 * i) as u16;
+                    execute!(stdout, cursor::MoveTo(row_pos, j), Print(cell)).unwrap();
+                }
+            }
+            for i in 12..=14 {
+                for j in 4..=5 {
+                    let cell = get_cell(&0);
+                    let row_pos = (2 * i) as u16;
+                    execute!(stdout, cursor::MoveTo(row_pos, j), Print(cell)).unwrap();
+                }
+            }
+        }
+        // render (1,1) to (11,21) is tetris grid.
+        {
+            execute!(stdout, cursor::MoveTo(1, 1)).unwrap();
             let mut rendering_grid_vec = grid.grid_vec.clone();
             for pos in &cur_tetris.poses {
                 *rendering_grid_vec.get_mut(pos.0).unwrap().get_mut(pos.1).unwrap() = cur_tetris.color;
@@ -46,7 +73,7 @@ fn main() {
                     row_string_vec.push(get_cell(cell));
                 }
                 let row_string = row_string_vec.join("");
-                execute!(stdout, cursor::MoveTo(0, index as u16), Print(row_string)).unwrap();
+                execute!(stdout, cursor::MoveTo(2, index as u16 + 1), Print(row_string)).unwrap();
             }
         }
 
@@ -97,6 +124,7 @@ fn main() {
         if drop {
             cur_tetris.drop_tetris(&grid.grid_vec);
             grid.apply_tetris(&cur_tetris);
+            cur_tetris = build::build_square_tetris(0, 0);
         }
         else if !cur_tetris.move_tetris(&grid.grid_vec, &shift) {
             grid.apply_tetris(&cur_tetris);
@@ -109,7 +137,7 @@ fn main() {
 }
 
 fn get_cell(cell: &usize) -> String {
-    let cell_uncolored = "███";
+    let cell_uncolored = "██";
     match cell {
         0 => return format!("{}", cell_uncolored.white()),
         1 => return format!("{}", cell_uncolored.red()),
